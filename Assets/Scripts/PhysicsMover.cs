8e6aaab94e7eb88f0e45a42cb9d2e13c9f447c59
keporusu <kepokeke1059@gmail.com>
2026-04-30 02:49:09 +0900

    簡単なアクション

---
+++ Assets/Scripts/PhysicsMover.cs
@@ -0,0 +1,86 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PhysicsMover : MonoBehaviour
+{
+    [SerializeField] private float gravity = 1.0f;
+    private Rigidbody2D rigidbody;
+    private Collider2D collider;
+    
+    private float accumulatedTime=0.0f;
+    private bool isAir = true;
+    private float jumpingPower = 0.0f;
+    
+    public bool IsAir => isAir;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
+        if (rigidbody == null || collider == null)
+        {
+            Debug.LogError("Rigidbody or Collider component is missing");
+            enabled = false;
+            return;
+        }
+    }
+    
+    private void FixedUpdate()
+    {
+        Vector2 movePoint = rigidbody.position;
+        
+        //空中ではないなら以下の処理をしない
+        if (!isAir) return;
+        
+        
+        if (jumpingPower > 0.0f)
+        {
+            //ジャンプ処理
+            movePoint += jumpingPower * Vector2.up;
+            jumpingPower = Mathf.Max(0.0f, jumpingPower-Time.fixedDeltaTime);
+        }
+        else
+        {
+            //落下処理
+            movePoint += accumulatedTime * gravity * Vector2.down;
+            accumulatedTime+=Time.fixedDeltaTime;
+        }
+        
+        
+        
+        //最終処理
+        rigidbody.MovePosition(movePoint);
+    }
+
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //ジャンプ中は足場無視
+        if(jumpingPower > 0.0f)return;
+        
+        Vector2 footPosition = rigidbody.position + new Vector2(0.0f, collider.bounds.extents.y);
+        RaycastHit2D hit = Physics2D.Raycast(footPosition, Vector2.up, 2.0f*this.collider.bounds.extents.y);
+        if (hit.collider != null)
+        {
+            isAir = false;
+            Vector2 groundedPosition = rigidbody.position + footPosition - hit.point;
+            rigidbody.MovePosition(groundedPosition);
+            Debug.Log("floor");
+        }
+    }
+
+    public void StartJump(float power)
+    {
+        isAir = true;
+        jumpingPower = power;
+        accumulatedTime = 0.0f;
+    }
+
+    public void StopJump()
+    {
+        jumpingPower = 0.1f;
+    }
+
+}
---
+++ Assets/Scripts/PlayeController.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayeController : MonoBehaviour
+{
+    [SerializeField] private float jumpPower = 1.0f;
+    private InputAction moveAction;
+    private InputAction jumpAction;
+    private PhysicsMover physicsMover;
+    
+    //State
+    private bool isJumping=false;
+
+    void Awake()
+    {
+        var inputActions = new InputSystem_Actions();
+        moveAction = inputActions.Player.Move;
+        jumpAction = inputActions.Player.Jump;
+    }
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        physicsMover = GetComponent<PhysicsMover>();
+        if (physicsMover == null)
+        {
+            Debug.LogError("Physics Mover component is missing");
+            enabled = false;
+        }
+    }
+
+    private void OnEnable()
+    {
+        jumpAction.Enable();
+        jumpAction.started += OnJumpStarted;
+        jumpAction.canceled += OnJumpCanceled;
+    }
+
+    private void OnDisable()
+    {
+        jumpAction.started -= OnJumpStarted;
+        jumpAction.canceled -= OnJumpCanceled;
+        jumpAction.Disable();
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void OnJumpStarted(InputAction.CallbackContext ctx)
+    {
+        if(physicsMover.IsAir)return;
+        isJumping = true;
+        physicsMover.StartJump(jumpPower);
+        Debug.Log("Jump started");
+    }
+
+    private void OnJumpCanceled(InputAction.CallbackContext ctx)
+    {
+        isJumping = false;
+        physicsMover.StopJump();
+        Debug.Log("Jump canceled");
+    }
+}
