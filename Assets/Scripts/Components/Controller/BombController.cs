d0e828d0de816cb1074d08584e6afff5133c3e7a
keporusu <kepokeke1059@gmail.com>
2026-08-14 19:08:39 +0900

    初期化順以外は実装完了

--- Assets/Scripts/Components/Controller/BombController.cs
+++ Assets/Scripts/Components/Controller/BombController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading;
 using Components.Combat.Attack;
 using Components.Detection;
+using Components.Identity;
 using Core.Constants;
 using Core.Exceptions;
 using Cysharp.Threading.Tasks;
@@ -15,6 +16,7 @@ namespace Components.Controller
     public class BombController : MonoBehaviour
     {
         [SerializeField] private float explodeTime = 1.0f;
+        [SerializeField] private float collisionTime = 0.2f;
         [SerializeField] private bool fireOnSpawn = false; //初手から着火しているか？
         [SerializeField] private float explosionRadius = 10.0f;
         [SerializeField] private float explosionDamage = 10.0f;
@@ -22,20 +24,51 @@ namespace Components.Controller
         //設定
         private AttackCollisionSetting _explosionCollisionSetting;
         
+        //キャッシュ
+        private CollisionManager _collisionManager_Cache;
+        private SpriteRenderer _spriteRenderer_Cache;
+        
         //状態
         private bool _isFire;
         
-        //キャッシュ
-        private VelocityAttackGenerator _velocityAttackGeneratorCache;
-        
         //キャンセル
         private CancellationTokenSource _cts;
-
+        
+        private void OnEnable()
+        {
+            
+            //ダメージを受けたら着火し、キャンセル付きのタイマーをスタート
+            var damageCollider = GetComponentsInChildren<Transform>()
+                                     .FirstOrDefault(obj => obj.gameObject.CompareTag(GameTags.DamageChannel))
+                                 ?? throw new MissingChannelException(GameTags.DamageChannel, gameObject.name);
+            if(!damageCollider.TryGetComponent(out DamageHitNotifier damagedNotifier))
+                throw new MissingComponentException($"[{GetType().Name}] DamageHitNotifier が {gameObject.name} に見つかりません");
+            damagedNotifier.OnHit += FireFromHit;
+            
+            
+            //自身が体当たりで攻撃したら爆発させる
+            if(!TryGetComponent(out VelocityAttackGenerator velocityAttackGenerator))
+                throw new MissingComponentException($"[{GetType().Name}] AttackController が {gameObject.name} に見つかりません");
+            velocityAttackGenerator.OnAttackVelocity += ExplodeFromHit;
+        }
+        private void OnDisable()
+        {
+            var damageCollider = GetComponentsInChildren<Transform>()
+                                     .FirstOrDefault(obj => obj.gameObject.CompareTag(GameTags.DamageChannel))
+                                 ?? throw new MissingChannelException(GameTags.DamageChannel, gameObject.name);
+            if(!TryGetComponent(out VelocityAttackGenerator velocityAttackGenerator))
+                throw new MissingComponentException($"[{GetType().Name}] VelocityAttackGenerator が {gameObject.name} に見つかりません");
+            velocityAttackGenerator.OnAttackVelocity -= ExplodeFromHit;
+        }
+        
         private void Start()
         {
             //キャッシュ
-            if(!TryGetComponent(out _velocityAttackGeneratorCache))
-                throw new MissingComponentException($"[{GetType().Name}] AttackController が {gameObject.name} に見つかりません");
+            if(!TryGetComponent(out _collisionManager_Cache))
+                throw new MissingComponentException($"[{GetType().Name}] CollisionManager が {gameObject.name} に見つかりません");
+            
+            if(!TryGetComponent(out _spriteRenderer_Cache))
+                throw new MissingComponentException($"[{GetType().Name}] SpriteRenderer が {gameObject.name} に見つかりません");
             
             //初期化
             _explosionCollisionSetting.shape = ColliderShape.Circle;
@@ -49,33 +82,6 @@ namespace Components.Controller
             }
         }
         
-        private void OnEnable()
-        {
-            
-            //ダメージを受けたら着火し、キャンセル付きのタイマーをスタート
-            var damageCollider = GetComponentsInChildren<Transform>()
-                                     .FirstOrDefault(obj => obj.gameObject.CompareTag(GameTags.DamageChannel))
-                                 ?? throw new MissingChannelException(GameTags.DamageChannel, gameObject.name);
-            if(!damageCollider.TryGetComponent(out DamageHitNotifier damagedNotifier))
-                throw new MissingComponentException($"[{GetType().Name}] DamageHitNotifier が {gameObject.name} に見つかりません");
-            damagedNotifier.OnHit += FireFromHit;
-            
-            //自身が体当たりで攻撃したら爆発させる
-            _velocityAttackGeneratorCache.OnAttackVelocity += Explode;
-        }
-        private void OnDisable()
-        {
-            var damageCollider = GetComponentsInChildren<Transform>()
-                                     .FirstOrDefault(obj => obj.gameObject.CompareTag(GameTags.DamageChannel))
-                                 ?? throw new MissingChannelException(GameTags.DamageChannel, gameObject.name);
-            if(!damageCollider.TryGetComponent(out DamageHitNotifier damagedNotifier))
-                throw new MissingComponentException($"[{GetType().Name}] DamageHitNotifier が {gameObject.name} に見つかりません");
-            damagedNotifier.OnHit -= FireFromHit;
-            
-            _velocityAttackGeneratorCache.OnAttackVelocity -= Explode;
-        }
-        
-        
         private void FireFromHit(Collider2D other)
         {
             Fire();
@@ -83,6 +89,9 @@ namespace Components.Controller
 
         private async void Fire()
         {
+            //着火済みなら着火しない
+            if(_isFire)return;
+            
             _isFire = true;
             _cts = new CancellationTokenSource();
             await FireAfterDelay(explodeTime, _cts.Token);
@@ -93,12 +102,28 @@ namespace Components.Controller
             await UniTask.Delay(System.TimeSpan.FromSeconds(delay), cancellationToken: token);
             Explode();
         }
+
+        private void ExplodeFromHit()
+        {
+            Explode();
+        }
         
-        private void Explode()
+        private async void Explode()
         {
             //発火による爆発はキャンセル
             _cts?.Cancel();
-            //_velocityAttackGeneratorCache.ActivateCollision(_explosionCollisionSetting);
+            var id = _collisionManager_Cache.GetAvailableCollisionId();
+            _collisionManager_Cache.ActivateCollision(id,gameObject,_explosionCollisionSetting,AttackPowerType.Radial);
+            _spriteRenderer_Cache.enabled = false;
+            await ExplodeAfterDelay(collisionTime);
+        }
+
+        private async UniTask ExplodeAfterDelay(float delay)
+        {
+            //数秒後に破壊処理
+            await UniTask.Delay(System.TimeSpan.FromSeconds(delay));
+            var root = EntityRoot.Require(this);
+            Destroy(root.gameObject);
         }
         
         
